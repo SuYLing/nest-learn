f471e32b6b4f541039ae9c6b0e50f1e1f5cc9ef9
YuLing <2017782140@qq.com>
2024-09-13 11:59:19 +0800

    chore: :hammer: project init

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+export default {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+}
